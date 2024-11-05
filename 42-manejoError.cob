81f2abe564b6345edfb029dd029c357926ffbe64
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2024-11-05 19:20:56 -0400

    Update and rename 44-IncDec.cob to 42-manejoError.cob

---
+++ 42-manejoError.cob
@@ -0,0 +1,50 @@
+⁠⁠       IDENTIFICATION DIVISION.
+       PROGRAM-ID. Capitulo42.
+
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       77  Numero PIC 99 VALUE ZEROS.
+       77  Multiplicador PIC 999 VALUE ZEROS.
+       77  Resultado PIC 9999 VALUE ZEROS.
+       77  Salida PIC XXXXX VALUE SPACES.
+
+       PROCEDURE DIVISION.
+
+       Inicio.
+       DISPLAY "Para salir introduce 'salir' en la consola.".
+       DISPLAY "Para multiplicar pulsa INTRO.".
+       ACCEPT Salida.
+       IF Salida = "salir"
+           PERFORM Finalizar
+       ELSE
+           PERFORM ReiniciaPrograma.
+           PERFORM IntroduceNumero.
+           PERFORM MostrarTabla.
+
+       Finalizar.
+       STOP RUN.
+
+       ReiniciaPrograma.
+       MOVE 0 TO Multiplicador.
+
+       IntroduceNumero.
+       DISPLAY "INTRODUCE UN NUMERO.".
+       ACCEPT Numero.
+       IF Numero IS ZEROS THEN
+           DISPLAY "Disculpe, debe introducir un número"
+           PERFORM Inicio
+       END-IF.
+
+       MostrarTabla.
+       DISPLAY "LA TABLA DEL " Numero ":".
+       PERFORM Calculos 10 TIMES.
+       PERFORM Inicio.
+
+       Calculos.
+       ADD 1 TO Multiplicador.
+       COMPUTE Resultado = Numero * Multiplicador.
+       DISPLAY Numero " * " Multiplicador " = " Resultado.
+
+       END PROGRAM Capitulo42.
+      
