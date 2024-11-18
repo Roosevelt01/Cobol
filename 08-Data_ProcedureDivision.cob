884dfa487bd72ab333476a976dbf37a5efac76a7
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2024-11-17 23:48:12 -0400

    Create 08-Data_ProcedureDivision.cob

---
+++ 08-Data_ProcedureDivision.cob
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CAPITULO-8.
+       
+       ENVIRONMENT DIVISION.
+       
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+           01 NUMERO PIC 99 VALUE 20.
+           
+       PROCEDURE DIVISION.
+           DISPLAY "Número: "NUMERO.
+           STOP RUN.
+       END PROGRAM CAPITULO-8.
